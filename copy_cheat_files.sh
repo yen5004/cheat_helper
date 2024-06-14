4ca30399a3a98bb16883388db42e1d18cc77fd83
yen5004 <119946331+yen5004@users.noreply.github.com>
2024-06-14 08:47:56 -0400

    Create copy_cheat_files.sh

---
+++ copy_cheat_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Use this script to get personal cheat files into the personal cheat folder
+# personal cheat files can come from the repo
+
+# Define variables
+#CHEAT_REPO_DIR=~/cheat_helper/cheatsheets/personal
+CHEAT_REPO_DIR=~/cheat_helper/personal_cheatsheets
+PERSONAL_CHEAT_DIR=~/.config/cheat/cheatsheets/personal
+
+# Ensure the personal cheat directory exists
+mkdir -p "$PERSONAL_CHEAT_DIR"
+
+# Copy custom cheat files to the personal directory
+cp "$CHEAT_REPO_DIR"/* "$PERSONAL_CHEAT_DIR"
+
+# Inform the user
+echo "Custom cheat files have been copied to $PERSONAL_CHEAT_DIR"
