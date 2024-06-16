bb28b35067a557ba6883b563fa21464e0fcc2ee4
yen5004 <119946331+yen5004@users.noreply.github.com>
2024-06-15 21:58:24 -0400

    Update personal_cheatsheets.sh

--- personal_cheatsheets.sh
+++ personal_cheatsheets.sh
@@ -14,25 +14,19 @@ CHEAT_SHEETS=(
     #["move"]="To move use: mv"
     #["remove"]="To remove use: rm"
     #["list"]="To list files use: ls"
-    ["custom"]="cd & ls: cdls = cd .. && ls \n verbose copy: cpv \n count = ls * | wc -l \n suno = sudo nano , n = nano , c = clear \n find sting in files = fstr <string> \n sudo last cmd = s \n easy extract = extract <file> "
+    ["custom"]="# cd & ls:  \n cdls = cd .. && ls \n # verbose copy:  \n cpv \n # count:  \n ls * | wc -l \n # suno:  \n sudo nano , # more custom:  \n cheat custom2"
+    ["custom2"]="# n: \n nano \n # c: clear \n # find sting in files: fstr <string> \n # sudo last cmd:  \n  s \n # easy extract: \n extract <file> "
     ["md5"]="# Calculate MD5 checksum of a file: \n md5sum file.txt \n
-# Verify MD5 checksums from a file: md5sum -c checksums.txt \n
-\n
-# Calculate MD5 checksums for multiple files: \n md5sum file1.txt file2.txt file3.txt
-\n
- # Calculate MD5 checksum for a binary file: \n md5sum -b binaryfile.bin
-\n
-# Calculate MD5 checksum of a string (input from echo): \n echo -n "Hello, world!" | md5sum
-\n
-# Calculate MD5 checksum of a string (input from a variable): \n my_string="This is a test string" \n 
-echo -n "$my_string" | md5sum
-\n
-# Calculate MD5 checksum of a string (input from a file): \n echo -n "My secret password" > secret.txt \n 
-md5sum secret.txt
-\n
-# Calculate MD5 checksum of a string and output in binary mode:  \n 
-echo -n "Binary data" | md5sum -b
-"
+    # Verify MD5 checksums from a file: md5sum -c checksums.txt \n
+    # Calculate MD5 checksums for multiple files: \n md5sum file1.txt file2.txt file3.txt \n
+    # Calculate MD5 checksum for a binary file: \n md5sum -b binaryfile.bin \n
+    # More md5sum cmds: \n md5more"
+    ["md5more"]="# Calculate MD5 checksum of a string (input from echo): \n echo -n "Hello, world!" | md5sum \n
+    # Calculate MD5 checksum of a string (input from a variable): \n my_string="This is a test string" \n 
+    echo -n "$my_string" | md5sum \n
+    # Calculate MD5 checksum of a string (input from a file): \n echo -n "My secret password" > secret.txt \n 
+    md5sum secret.txt \n
+    # Calculate MD5 checksum of a string and output in binary mode:  \n echo -n "Binary data" | md5sum -b "
 
 )
 
